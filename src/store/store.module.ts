3fc022a12102f6ef9e1a21ddf484e46d88289fcf
Julianaba7 <mj.ballesteros@uniandes.edu.co>
2026-03-19 13:06:58 -0500

    store

--- src/store/store.module.ts
+++ src/store/store.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Store } from './entities/store.entity';
 import { StoreController } from './store.controller';
 import { StoreService } from './store.service';
+import { StorageModule } from '../storage/storage.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Store])],
+  imports: [TypeOrmModule.forFeature([Store]), StorageModule],
   controllers: [StoreController],
   providers: [StoreService],
   exports: [StoreService],
