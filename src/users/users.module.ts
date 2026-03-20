7e8e25a0406e587d2159ecb9ceb3a3864537fff8
santi-pine03 <s.pinedaq@uniandes.edu.co>
2026-03-20 11:54:52 -0500

    Edit profile changes

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { StorageModule } from '../storage/storage.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), StorageModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
