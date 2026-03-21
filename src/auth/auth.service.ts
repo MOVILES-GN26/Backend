16df338b9760386579a275ea4ff76056b374fe36
santi-pine03 <s.pinedaq@uniandes.edu.co>
2026-03-20 20:05:04 -0500

    ncf changes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,6 +8,7 @@ import * as bcrypt from 'bcrypt';
 import { UsersService } from '../users/users.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
@@ -81,6 +82,25 @@ export class AuthService {
         { sub: userId, email, type: 'refresh' },
         { expiresIn: '7d' },
       ),
+    };
+  }
+  async nfcLogin(userId: string) {
+    const user = await this.usersService.findById(userId);
+    if (!user) {
+      throw new NotFoundException('User not found.');
+    }
+
+    const tokens = this.generateTokens(user.id, user.email);
+
+    return {
+      ...tokens,
+      user: {
+        id: user.id,
+        email: user.email,
+        first_name: user.first_name,
+        last_name: user.last_name,
+        major: user.major,
+      },
     };
   }
 }
